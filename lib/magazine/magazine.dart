491aa3ee2e731c59d0b88dc8319d74f325a7c2f2
lou <habigkys@naver.com>
2022-12-10 15:26:47 +0900

    매거진 리스트
    Signed-off-by: lou <habigkys@naver.com>

--- lib/magazine/magazine.dart
+++ lib/magazine/magazine.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:imitate_sirloin/appbar/baseappbar.dart';
+import 'package:http/http.dart' as http;
 
 class MagazinePage extends StatelessWidget {
   const MagazinePage({super.key});
@@ -14,7 +15,7 @@ class MagazinePage extends StatelessWidget {
         child: ListView(
           children: <Widget>[
             Container(
-              height: 200.0,
+              height: 400.0,
               margin: const EdgeInsets.all(30.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -22,10 +23,52 @@ class MagazinePage extends StatelessWidget {
                   width: 1,
                   color: Colors.grey,
                 ),
+                borderRadius: BorderRadius.circular(10),
+                image : const DecorationImage(
+                  image: AssetImage("images/1.webp"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 270.0,
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('무슨 냄새 안 나요? 치즈 향 품은 고기',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('드라이에이징 고기 향의 정체를 알아봅니다.',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(''),
+                        Text('2022.11.22',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 200.0,
+              height: 400.0,
               margin: const EdgeInsets.all(30.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -33,10 +76,52 @@ class MagazinePage extends StatelessWidget {
                   width: 1,
                   color: Colors.grey,
                 ),
+                borderRadius: BorderRadius.circular(10),
+                image : const DecorationImage(
+                  image: AssetImage("images/2.webp"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 270.0,
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('숙성人류, 누가 시작 했을까?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('숙성의 맛, 고대부터 흘러오다.',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(''),
+                        Text('2022.10.11',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 200.0,
+              height: 400.0,
               margin: const EdgeInsets.all(30.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -44,10 +129,55 @@ class MagazinePage extends StatelessWidget {
                   width: 1,
                   color: Colors.grey,
                 ),
+                borderRadius: BorderRadius.circular(10),
+                image : const DecorationImage(
+                  image: AssetImage("images/3.webp"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 270.0,
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('철판 요리를 일본 코스에 녹이다',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text('섬세함을 담은 텟판야키 산고쇼, 이상철 셰프',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(''),
+                        Text('2022.05.17',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 200.0,
+              height: 400.0,
               margin: const EdgeInsets.all(30.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -55,10 +185,55 @@ class MagazinePage extends StatelessWidget {
                   width: 1,
                   color: Colors.grey,
                 ),
+                borderRadius: BorderRadius.circular(10),
+                image : const DecorationImage(
+                  image: AssetImage("images/4.webp"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 270.0,
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('안심의 숨겨진 매력, 아시나요?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text('부드러울 뿐 아니라 쫄깃 쫀득하죠',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(''),
+                        Text('2022.05.14',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 200.0,
+              height: 400.0,
               margin: const EdgeInsets.all(30.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
@@ -66,11 +241,58 @@ class MagazinePage extends StatelessWidget {
                   width: 1,
                   color: Colors.grey,
                 ),
+                borderRadius: BorderRadius.circular(10),
+                image : const DecorationImage(
+                  image: AssetImage("images/5.webp"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 270.0,
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('첸트로다움이 만든 차이',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text('다채로운 변화 첸트로, 도웅희 셰프',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(''),
+                        Text('2022.04.25',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
     );
+
+    
   }
 }
