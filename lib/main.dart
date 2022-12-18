39254c78eb6b490298971f71d5d1c9a56aee73d8
lou <habigkys@naver.com>
2022-12-18 19:57:54 +0900

    firebase + dynamiclink + 메시징 앱푸시 도전
    Signed-off-by: lou <habigkys@naver.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:imitate_sirloin/controllers/AppController.dart';
+import 'package:imitate_sirloin/firebase_options.dart';
 import 'package:imitate_sirloin/home/home.dart';
 import 'package:imitate_sirloin/category/category.dart';
+import 'package:imitate_sirloin/link/dynamiclink.dart';
 import 'package:imitate_sirloin/magazine/magazinelist.dart';
 import 'package:imitate_sirloin/my/my.dart';
 import 'package:imitate_sirloin/search/search.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future requestPermission() async {
+  NotificationSettings settings = await _messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  var fcmToken = await FirebaseMessaging.instance.getToken(vapidKey: "BNUUSlqrxujsOKVF3k8J2L1PsCq-9QchcwTlT5LiR8RrDyX-IpjZSoAt8d5KGJVqh-1CqufNksmVbBNCZvZBSZA");
+  DynamicLink().setup();
   runApp(const MyApp());
 }
 
@@ -36,6 +61,7 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final AppController c = Get.put(AppController());
 
   int _selectedIndex = 0;
   static const TextStyle optionStyle = TextStyle(
@@ -60,8 +86,24 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      body: FutureBuilder(
+        future: c.initialize(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData && snapshot.connectionState == ConnectionState.done){
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('title', style: TextStyle(fontSize: 20)),
+                  Text('message', style: TextStyle(fontSize: 15)),
+                ],
+              ));
+          }else{
+            return SafeArea(
+              child: _widgetOptions.elementAt(_selectedIndex),
+            );
+          }
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import desktop_webview_auth
+import firebase_auth
 import firebase_core
+import firebase_database
+import firebase_messaging
+import flutter_local_notifications
 import path_provider_macos
 import share_plus
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
